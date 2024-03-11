348b18aff2160478c9f9dfcb25a11afc7055bd48
Chau Thanh Dat <chd9hc@bosch.com>
2024-03-11 11:11:29 +0700

    Update Reference Document

--- Core/Inc/Can_Interface.h
+++ Core/Inc/Can_Interface.h
@@ -13,9 +13,110 @@
 #define LIGHT_CONTROL_NODE 0x01
 #define OPTION_NODE	0x02
 
+/******************************************************************************
+* Function : CAN_IF_Transmit_UDS_Request(uint8_t Node, uint8_t UDS_Req)
+*//**
+* \b Description:
+*
+* This function is used to send UDS_Req code to Node
+*
+* PRE-CONDITION:  Can Peripheral enable
+*
+* POST-CONDITION: None
+*
+* @param [in]     uint32_t
+*
+* @return 		   HAL_StatusTypeDef
+*
+* \b Example Example:
+* @code
+* 	CAN_IF_Transmit_UDS_Request(uint8_t Node, uint8_t UDS_Req);
+*
+* @endcode
+*
+* @see CAN_IF_Transmit_UDS_Request(uint8_t Node, uint8_t UDS_Req)
+*
+*******************************************************************************/
 HAL_StatusTypeDef CAN_IF_Transmit_UDS_Request(uint8_t Node, uint8_t UDS_Req);
+
+/******************************************************************************
+* Function : CAN_IF_Trasmit_Data_Frame(uint8_t Node, const uint8_t *DataBuffer, uint8_t DataLength);
+*//**
+* \b Description:
+*
+* This function is used to send DataBuffer with DataLength to Node
+*
+* PRE-CONDITION:  Can Peripheral enable
+*
+* POST-CONDITION: None
+*
+* @param [in]     uint8_t 	Node to send data
+* @param [in]     uint8_t* 	Pointer to buffer
+* @param [in]     uint8_t	DataLength of buffer
+*
+* @return 		   HAL_StatusTypeDef
+*
+* \b Example Example:
+* @code
+* 	CAN_IF_Trasmit_Data_Frame(uint8_t Node, const uint8_t *DataBuffer, uint8_t DataLength);
+*
+* @endcode
+*
+* @see CAN_IF_Trasmit_Data_Frame(uint8_t Node, const uint8_t *DataBuffer, uint8_t DataLength);
+*
+*******************************************************************************/
 HAL_StatusTypeDef CAN_IF_Trasmit_Data_Frame(uint8_t Node, const uint8_t *DataBuffer, uint8_t DataLength);
+
+/******************************************************************************
+* Function : CAN_IF_Receive_UDS_Respond(uint8_t *UDS_Req)
+*//**
+* \b Description:
+*
+* This function is used to receive UDS code
+*
+* PRE-CONDITION:  Can Peripheral enable
+*
+* POST-CONDITION: None
+*
+* @param [in]     uint8_t*
+*
+* @return 		  HAL_StatusTypeDef
+*
+* \b Example Example:
+* @code
+* 	CAN_IF_Receive_UDS_Respond(uint8_t *UDS_Req);
+*
+* @endcode
+*
+* @see CAN_IF_Receive_UDS_Respond(uint8_t *UDS_Req)
+*
+*******************************************************************************/
 HAL_StatusTypeDef CAN_IF_Receive_UDS_Respond(uint8_t *UDS_Req);
+
+/******************************************************************************
+* Function : BL_ReadAddressData(uint32_t address)
+*//**
+* \b Description:
+*
+* This function is used to receive data from fifo0
+*
+* PRE-CONDITION:  Can Peripheral enable
+*
+* POST-CONDITION: None
+*
+* @param [in]     uint8_t* Pointer to buffer Rx
+*
+* @return 		   HAL_StatusTypeDef
+*
+* \b Example Example:
+* @code
+* 	CAN_IF_Receive_Data_Frame(uint8_t *DataBuffer);
+*
+* @endcode
+*
+* @see CAN_IF_Receive_Data_Frame(uint8_t *DataBuffer)
+*
+*******************************************************************************/
 HAL_StatusTypeDef CAN_IF_Receive_Data_Frame(uint8_t *DataBuffer);
 
 #endif /* INC_CAN_INTERFACE_H_ */
